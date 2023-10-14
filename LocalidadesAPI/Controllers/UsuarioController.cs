9f736aebbecde7ecf0d779e30b5c4f9b5879d8e0
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-10-14 17:55:31 -0300

    :sparkles: Implementada a função de login

--- LocalidadesAPI/Controllers/UsuarioController.cs
+++ LocalidadesAPI/Controllers/UsuarioController.cs
@@ -18,15 +18,16 @@ namespace LocalidadesAPI.Controllers
         }
 
         [HttpPost]
+        [Route("/Usuario/Cadastrar")]
         public async Task<IActionResult> Cadastrar(string email, string senha)
         {
+            if (!ValidacaoHelper.ValidarEmail(email) || !ValidacaoHelper.ValidarSenha(senha))
+                return BadRequest("E-mail e/ou senha inválido!");
+
             var usuarioExiste = await _usuarioRepository.ObterUsuarioPorEmail(email);
 
             if (usuarioExiste != null)
                 return BadRequest("Já existe um usuário cadastrado à esse e-mail!");
-
-            if (!ValidacaoHelper.ValidarEmail(email) || !ValidacaoHelper.ValidarSenha(senha))
-                return BadRequest("E-mail e/ou senha inválido!");
 
             var usuario = new Usuario()
             {
@@ -37,9 +38,27 @@ namespace LocalidadesAPI.Controllers
             var retorno = await _usuarioRepository.Inserir(usuario);
 
             if (retorno == 0)
-                return StatusCode(500);
+                return StatusCode(500, "Erro ao cadastrar usuário!");
 
-            return StatusCode(201);
+            return StatusCode(201, "Usuário cadastrado com sucesso!");
+        }
+
+        [HttpPost]
+        [Route("/Usuario/Login")]
+        public async Task<IActionResult> Login(string email, string senha)
+        {
+            if (!ValidacaoHelper.ValidarEmail(email) || !ValidacaoHelper.ValidarSenha(senha))
+                return BadRequest("E-mail e/ou senha inválido!");
+
+            var usuarioExiste = await _usuarioRepository.ObterUsuarioPorEmail(email);
+
+            if (usuarioExiste == null)
+                return NotFound("Não existe nenhum usuário cadastrado à esse e-mail!");
+
+            if (usuarioExiste.Email == email && usuarioExiste.Senha == Criptografia.GerarHash(senha))
+                return Ok();
+
+            return BadRequest("Credenciais inválidas!");
         }
     }
 }
