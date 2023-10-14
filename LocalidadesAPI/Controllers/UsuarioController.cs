ff24cbb6c5bfae579a56be7d876b41c78eb292f8
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-10-14 16:33:02 -0300

    :sparkles: Implementado o cadastro de usuário

---
+++ LocalidadesAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using LocalidadesAPI.Helpers;
+using LocalidadesAPI.Models;
+using LocalidadesAPI.Repositories;
+using LocalidadesAPI.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalidadesAPI.Controllers
+{
+    [ApiController]
+    [Route("/Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IConfiguration configuration)
+        {
+            _usuarioRepository = new UsuarioRepository(configuration);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(string email, string senha)
+        {
+            var usuarioExiste = await _usuarioRepository.ObterUsuarioPorEmail(email);
+
+            if (usuarioExiste != null)
+                return BadRequest("Já existe um usuário cadastrado à esse e-mail!");
+
+            if (!ValidacaoHelper.ValidarEmail(email) || !ValidacaoHelper.ValidarSenha(senha))
+                return BadRequest("E-mail e/ou senha inválido!");
+
+            var usuario = new Usuario()
+            {
+                Email = email,
+                Senha = Criptografia.GerarHash(senha)
+            };
+
+            var retorno = await _usuarioRepository.Inserir(usuario);
+
+            if (retorno == 0)
+                return StatusCode(500);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ LocalidadesAPI/Helpers/ValidacaoHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LocalidadesAPI.Helpers
+{
+    public static class ValidacaoHelper
+    {
+        public static bool ValidarEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) 
+                return false;
+
+            string emailRegex = string.Format("{0}{1}",
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))",
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
+
+            return Regex.IsMatch(email, emailRegex);
+        }
+
+        public static bool ValidarSenha(string senha)
+        {
+            if (string.IsNullOrEmpty(senha) || senha.Length < 6)
+                return false;
+
+            return true;
+        }
+    }
+}
