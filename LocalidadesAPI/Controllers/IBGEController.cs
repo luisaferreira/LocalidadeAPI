5fda2d8d0573f255ead094fc0341c522139dd37c
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-10-14 19:11:06 -0300

    :recycle: Ajuste na mensagem de retorno

---
+++ LocalidadesAPI/Controllers/IBGEController.cs
@@ -0,0 +1,41 @@
+﻿using LocalidadesAPI.Helpers;
+using LocalidadesAPI.Models;
+using LocalidadesAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalidadesAPI.Controllers
+{
+    [ApiController]
+    public class IBGEController : Controller
+    {
+        private readonly IBGERepository _IBGERepository;
+
+        public IBGEController(IConfiguration configuration)
+        {
+            _IBGERepository = new IBGERepository(configuration);
+        }
+
+        [HttpPost]
+        [Route("/Localidade/Adicionar")]
+        public async Task<IActionResult> AdicionarLocalidade(IBGE ibge)
+        {
+            if (ibge == null)
+                return BadRequest("Objeto não pode ser nulo!");
+
+            if (!ValidacaoHelper.ValidarCodigoIBGE(ibge.Codigo) || !ValidacaoHelper.ValidarSiglaEstado(ibge.Estado) || string.IsNullOrEmpty(ibge.Cidade))
+                return BadRequest("Informações inválidas!");
+
+            var localidadeExiste = await _IBGERepository.ObterPorCodigo(ibge.Codigo);
+
+            if (localidadeExiste != null)
+                return BadRequest("Já existe uma localidade vinculada à esse código IBGE!");
+
+            var retorno = await _IBGERepository.Inserir(ibge);
+
+            if (retorno == 0)
+                return StatusCode(500, "Erro ao adicionar localidade!");
+
+            return StatusCode(201, "Localidade adicionada com sucesso!");
+        }
+    }
+}
