6e476f79c85af6f12a44284ad57da5a36692f4f1
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-10-14 14:56:16 -0300

    :sparkles: Implementado o repositório de usuário

---
+++ LocalidadesAPI/Interfaces/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using LocalidadesAPI.Interfaces.Repositories.Base;
+using LocalidadesAPI.Models;
+
+namespace LocalidadesAPI.Interfaces.Repositories
+{
+    public interface IUsuarioRepository : IBaseRepository<Usuario>
+    {
+        Task<Usuario> ObterUsuarioPorEmail(string email);
+    }
+}
---
+++ LocalidadesAPI/Repositories/UsuarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using LocalidadesAPI.Interfaces.Repositories;
+using LocalidadesAPI.Models;
+using LocalidadesAPI.Repositories.Base;
+using System.Text;
+
+namespace LocalidadesAPI.Repositories
+{
+    public class UsuarioRepository : BaseRepository<Usuario>, IUsuarioRepository
+    {
+        public UsuarioRepository(IConfiguration configuration) 
+            : base(configuration) { }
+
+        public async Task<Usuario> ObterUsuarioPorEmail(string email)
+        {
+            var parametros = new Dictionary<string, object>()
+            {
+                { "Email", email }
+            };
+
+            var sql = new StringBuilder();
+            sql.AppendLine("SELECT * FROM USUARIO");
+            sql.AppendLine("WHERE EMAIL = @Email");
+
+            var retorno = await CustomRepository.GetAsync(sql.ToString(), parametros);
+
+            return retorno?.FirstOrDefault();
+        }
+    }
+}
